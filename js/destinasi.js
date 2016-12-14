03363b269bf3fd84c95f5796b9f293f82f2e5ae6
cahyowhy <cahyowhy01@gmail.com>
2016-12-14 12:54:12 +0700

    another commit

--- js/destinasi.js
+++ js/destinasi.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
-	var $window = $(window);
-	var $document = $(document);
-	$('#obrollan').click(function() {
-		$("#obrollan-content").toggle();
-	});
-	$(window).on('scroll',function(){
-		if($window.scrollTop() + $window.height() > $document.height() - 100) {
-			$('#obrollan').hide();
-		}
-		else{
-			$('#obrollan').show();			
-		}
-	});
+    var $window = $(window);
+    var $document = $(document);
+    $('#obrollan').click(function() {
+        $("#obrollan-content").toggle();
+    });
+    $('.obrollan-item-parent').click(function() {
+        $("#obrollan-user").css('display','block');
+    });
+    $('#obrollan-user-content').click(function() {
+        $(this).toggle();
+    });
+
+    $("#obrollan-user").click(function() { $("#obrollan-user").toggle() });
+
+    $(window).on('scroll', function() {
+        if ($window.scrollTop() + $window.height() > $document.height() - 100) {
+            $('#obrollan').hide();
+            $('#obrollan-user').hide();
+        } else {
+            $('#obrollan').show();
+        }
+    });
 });

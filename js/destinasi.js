cf67708ca6530ce0fbbdd6adaebd3d3380d2edfa
cahyowhy <cahyowhy01@gmail.com>
2016-12-17 04:41:14 +0700

    ok

--- js/destinasi.js
+++ js/destinasi.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
+
     var $window = $(window);
     var $document = $(document);
     $('#obrollan').click(function() {
         $("#obrollan-content").toggle();
     });
     $('.obrollan-item-parent').click(function() {
-        $("#obrollan-user").css('display','block');
+        $("#obrollan-user").css('display', 'block');
     });
     $('#obrollan-user-content').click(function() {
         $(this).toggle();
     });
+
 
     $("#obrollan-user").click(function() { $("#obrollan-user").toggle() });
 
@@ -21,4 +23,75 @@ $(document).ready(function() {
             $('#obrollan').show();
         }
     });
+
+    var apiURL = "api/destinasi.json";
+    var demo = new Vue({
+
+        el: '.item-container',
+
+        data: {
+            items: null
+        },
+
+        created: function() {
+            this.fetchData()
+        },
+
+        methods: {
+            fetchData: function() {
+                /*var xhr = new XMLHttpRequest()
+                var self = this
+                xhr.open('GET', apiURL)
+                xhr.onload = function() {
+                    self.items = JSON.parse(xhr.responseText)
+                    console.log(self.items[0].judul)
+                }
+                xhr.send()*/
+                var self = this;
+                $.ajax({
+                        url: 'api/destinasi.json',
+                        type: 'get',
+                        dataType: 'json',
+
+                    })
+                    .done(function(response) {
+                        self.items = response;
+                        console.log(response);
+                        /*$('.item-container').imagesLoaded(function() {
+                            $('.item-container').masonry({
+                                columnWidth: '.item',
+                                itemSelector: '.item'
+                            });
+                        });*/
+                        $('.item-container').imagesLoaded()
+
+                        .done(function(instance) {
+                            $('.item-container').masonry({
+                                columnWidth: '.item',
+                                itemSelector: '.item'
+                            });
+                        })
+                        .progress(function(instance) {
+                            $('.item-container').masonry({
+                                columnWidth: '.item',
+                                itemSelector: '.item'
+                            });
+                        })
+                        .always(function(instance) {
+                            $('.item-container').masonry({
+                                columnWidth: '.item',
+                                itemSelector: '.item'
+                            });
+                        })
+                    })
+                    .fail(function() {
+                        console.log("error");
+                    })
+                    .always(function() {
+                        console.log("complete");
+                    });
+
+            }
+        }
+    })
 });
